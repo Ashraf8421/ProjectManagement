c8e68e2e87641f8d1e636980c62715643cb79edd
Ashraf <ashraff1904@gmail.com>
2026-03-15 12:56:25 +0530

    add models

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+//basic configurations
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+
+//cors configurations
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhots:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+  }),
+);
+
+//import the routes
+import healthCheckRouter from "./routes/healthcheck.routes.js";
+app.use("/api/v1/healthcheck", healthCheckRouter);
+
+app.get("/", (req, res) => {
+  res.send("welcome to basecamp");
+});
+
+export default app;
+
+app.use 
