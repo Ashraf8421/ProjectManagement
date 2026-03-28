d6859a09d5b1a395235681b687f5dbfad1f54546
Ashraf <ashraff1904@gmail.com>
2026-03-29 00:43:34 +0530

    Add Task Controllers

---
+++ src/controllers/task.controllers.js
@@ -0,0 +1,166 @@
+import { User } from "../models/user.model.js";
+import { Project } from "../models/project.models.js";
+import { Task } from "../models/task.models.js";
+import { SubTask } from "../models/subtask.model.js";
+import { ApiError } from "../utils/api-error.js";
+import { asyncHandler } from "../utils/async-handler.js";
+import mongoose from "mongoose";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
+
+const getTasks = async(async (req, res) => {
+  const { projectId } = req.params;
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const tasks = await Task.find({
+    project: new mongoose.Types.ObjectId(projectId),
+  }).populate("assignedTo", "avatar username fullname");
+
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created successfully"));
+});
+
+const createTask = async(async (req, res) => {
+  const { title, description, assignedTo, status } = req.body;
+  const { projectId } = req.params;
+  const project = await Project.findById(projectId);
+  if (!project) {
+    throw new ApiError(404, "Project not found");
+  }
+
+  const files = req.files || [];
+
+  const attachments = files.map((file) => {
+    return {
+      url: `${process.env.SERVER_URL}/images/${file.originalname}`,
+      mimeType: file.mimeType,
+      size: file.size,
+    };
+  });
+
+  const task = await Task.create({
+    title,
+    description,
+    project: new mongoose.Types.ObjectId(projecId),
+    assignedTo: assignedTo
+      ? new mongoose.Types.ObjectId(assignedTo)
+      : undefined,
+    status,
+    assignedBy: new mongoose.Types.ObjectId(req.user._id),
+    attachments,
+  });
+  return res
+    .status(201)
+    .json(new ApiResponse(201, task, "Task created successfully"));
+});
+
+const getTaskById = async(async (req, res) => {
+  const { taskId } = req.params;
+
+  const task = await Task.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(taskId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "assignedTo",
+        foreignField: "_id",
+        as: "assignedTo",
+        pipeline: [
+          {
+            _id: 1,
+            username: 1,
+            fullName: 1,
+            avatar: 1,
+          },
+        ],
+      },
+    },
+    {
+      $lookup: {
+        from: "subtasks",
+        localField: "_id",
+        foreignField: "task",
+        as: "subtasks",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "createdBy",
+              foreignField: "_id",
+              as: "createdBy",
+              pipeline: [
+                {
+                  $project: {
+                    _id: 1,
+                    username: 1,
+                    fullName: 1,
+                    avatar: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $addFields: {
+              createdBy: {
+                $arrayElemAt: ["$createdBy", 0],
+              },
+            },
+          },
+        ],
+      },
+    },
+    {
+      $addFields: {
+        assignedTo: {
+          $arrayElemAt: ["$assignedTo", 0],
+        },
+      },
+    },
+  ]);
+
+  if (!task || task.length === 0) {
+    throw new ApiError(404, "Task not found");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, task[0], "Task fetched successfully"));
+});
+
+const updateTask = async(async (req, res) => {
+  //chai
+});
+
+const deleteTask = async(async (req, res) => {
+  //chai
+});
+
+const createSubTask = async(async (req, res) => {
+  //chai
+});
+
+const updateSubTask = async(async (req, res) => {
+  //chai
+});
+
+const deleteSubTask = async(async (req, res) => {
+  //chai
+});
+
+export {
+  createSubTask,
+  createTask,
+  deleteSubTask,
+  deleteTask,
+  getTaskById,
+  getTasks,
+  updateSubTask,
+  updateTask,
+};
---
+++ src/routes/project.routes.js
@@ -0,0 +1,58 @@
+import { Router } from "express";
+import {
+  addMembersToProject,
+  createProject,
+  deleteMember,
+  getProjectById,
+  getProjects,
+  getProjectMembers,
+  updateMemberRole,
+  updateProject,
+  deleteProject,
+} from "../controllers/project.controllers.js";
+import { validate } from "../middlewares/validator.middleware.js";
+import {
+  createProjectValidator,
+  addMembertoProjectValidator,
+} from "../validators/index.js";
+import {
+  verifyJWT,
+  validateProjectPermission,
+} from "../middlewares/auth.middelware.js";
+import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
+
+const router = Router();
+router.use(verifyJWT);
+
+router
+  .route("/")
+  .get(getProjects)
+  .post(createProjectValidator(), validate, createProject);
+
+router
+  .route("/:projectId")
+  .get(validateProjectPermission(AvailableUserRole), getProjectById)
+  .put(
+    validateProjectPermission([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]),
+    createProjectValidator(),
+    validate,
+    updateProject,
+  )
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteProject);
+
+router
+  .route("/:projecId/members")
+  .get(getProjectMembers)
+  .post(
+    validateProjectPermission([UserRolesEnum.ADMIN]),
+    addMembertoProjectValidator(),
+    validate,
+    addMembersToProject,
+  );
+
+router
+  .route("/:projecId/members/:userId")
+  .put(validateProjectPermission([UserRolesEnum.ADMIN]), updateMemberRole)
+  .delete(validateProjectPermission([UserRolesEnum.ADMIN]), deleteMember);
+
+export default router;
